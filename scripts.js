5e4043beca66d98dfa9babdc981d43448b7ea4e4
lidialima <lidialima2lima@gmail.com>
2024-09-06 22:02:41 -0300

    DevClub Jogo pedra, papel, tesoura

---
+++ scripts.js
@@ -0,0 +1,40 @@
+const result = document.querySelector('.result')
+const humanScore = document.querySelector('#human-score')
+const machineScore = document.querySelector('#machine-score')
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+const playHuman = (humanChoice) => {
+
+    plauTheGame (humanChoice, playMachine())
+}
+
+const playMachine = () => {
+    const choices = ['rock', 'paper', 'scissors']
+    const randomnumber = Math.floor(Math.random() * 3)
+
+    return choices [randomnumber]
+}
+
+const plauTheGame = (human, machine) => {
+
+    console.log('Humano: ' + human + ' Maquina: ' + machine)
+
+    if(human === machine){
+        result.innerHTML = "Deu empate &#x1F47D"
+    }else if( (human === 'paper' && machine === 'rock') || 
+              (human === 'rock' && machine === 'scissors') || 
+              (human === 'scissors' && machine === 'paper') 
+){
+        humanScoreNumber++
+        humanScore.innerHTML = humanScoreNumber
+        result.innerHTML = "Você ganhou! &#x1F61C"
+
+
+    }else {
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
+        result.innerHTML = "Você perdeu &#x1F62D"
+    }
+}
